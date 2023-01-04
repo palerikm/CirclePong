a80651cf75aa9b02829a8c4375a351326f1049a6
Pal <palmarti@cisco.com>
2023-01-04 11:31:19 +0100

    Initial Commit

---
+++ CirclePong/CirclePongApp.swift
@@ -0,0 +1,17 @@
+//
+//  CirclePongApp.swift
+//  CirclePong
+//
+//  Created by Pål-Erik Martinsen on 04/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct CirclePongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
